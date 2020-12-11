83b9fc3ba4f6c581d54a022a89a0a5e70b9ad3e5
Ehuntwork <sir.mle.learnsalot@gmail.com>
2020-12-11 05:59:48 -0800

    Added RaceEthnicityInfo component to IntakePacket.jsx

--- src/components/pages/IntakePacket.jsx
+++ src/components/pages/IntakePacket.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import IdVerification from './IntakePacketContent/BySupervisor/IdVerification';
 import ContactInfo from './IntakePacketContent/ByGuests/ContactInfo';
 import FamilyMembers from './IntakePacketContent/ByGuests/FamilyMembers';
+import RaceEthnicityInfo from './IntakePacketContent/ByGuests/RaceEthnicityInfo';
 const IntakePacket = () => {
   return (
     <div className="container">
       <h1>Intake Packet</h1>
       <ContactInfo />
       <FamilyMembers />
+      <RaceEthnicityInfo />
       <IdVerification />
     </div>
   );
