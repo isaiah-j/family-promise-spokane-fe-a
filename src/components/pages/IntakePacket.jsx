828fb40874593c8f8eedfbbcfd977bf369766779
Ehuntwork <sir.mle.learnsalot@gmail.com>
2020-12-14 07:57:09 -0800

    added switch/case for multiStep form navigation

--- src/components/pages/IntakePacket.jsx
+++ src/components/pages/IntakePacket.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useForm, useStep } from 'react-hooks-helper';
+
 import IdVerification from './IntakePacketContent/BySupervisor/IdVerification';
 import ContactInfo from './IntakePacketContent/ByGuests/ContactInfo';
 import FamilyMembers from './IntakePacketContent/ByGuests/FamilyMembers';
@@ -8,21 +10,43 @@ import ChildSchoolInfo from './IntakePacketContent/ByGuests/ChildSchoolInfo';
 import DomesticViolence from './IntakePacketContent/ByGuests/DomesticViolence';
 import HomelessHistory from './IntakePacketContent/ByGuests/HomelessHistory';
 import Insurance from './IntakePacketContent/ByGuests/Insurance';
+const steps = [
+  { id: 'ContactInfo' },
+  { id: 'FamilyMembers' },
+  { id: 'RaceEthnicityInfo' },
+  { id: 'BarriersPage' },
+  { id: 'ChildSchoolInfo' },
+  { id: 'IdVerification' },
+  { id: 'DomesticViolence' },
+  { id: 'HomelessHistory' },
+  { id: 'Insurance' },
+];
 const IntakePacket = () => {
-  return (
-    <div className="container">
-      <h1>Intake Packet</h1>
-      <ContactInfo />
-      <FamilyMembers />
-      <RaceEthnicityInfo />
-      <BarriersPage />
-      <ChildSchoolInfo />
-      <IdVerification />
-      <DomesticViolence />
-      <HomelessHistory />
-      <Insurance />
-    </div>
-  );
+  const { step, navigation } = useStep({ initialStep: 0, steps });
+  const { id } = step;
+  const props = { navigation };
+  switch (id) {
+    case 'ContactInfo':
+      return <ContactInfo {...props} />;
+    case 'FamilyMembers':
+      return <FamilyMembers {...props} />;
+    case 'RaceEthnicityInfo':
+      return <RaceEthnicityInfo {...props} />;
+    case 'BarriersPage':
+      return <BarriersPage {...props} />;
+    case 'ChildSchoolInfo':
+      return <ChildSchoolInfo {...props} />;
+    case 'IdVerification':
+      return <x {...props} />;
+    case 'DomesticViolence':
+      return <DomesticViolence {...props} />;
+    case 'HomelessHistory':
+      return <HomelessHistory {...props} />;
+    case 'Insurance':
+      return <Insurance {...props} />;
+    default:
+      return null;
+  }
 };
 
 export default IntakePacket;
